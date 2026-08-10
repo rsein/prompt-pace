815670a32b912b96b5f816ab91353a2849964fd9
rsein <rodrigo@rseinestudio.com>
2026-08-10 09:49:40 -0300

    Melhoria Poster, Varios narradores

--- supabase/migrations/008_narrator_style.sql
+++ supabase/migrations/008_narrator_style.sql
@@ -3,4 +3,4 @@
 
 alter table public.journeys
   add column if not exists narrator_style text not null default 'engracado'
-  check (narrator_style in ('engracado','serio','chefe','treinador','pastor','simpatico','corporativo','locutor','apresentador'));
+  check (narrator_style in ('engracado','serio','chefe','treinador','pastor','simpatico','corporativo','locutor','apresentador', 'sindico de condomínio', 'professor', 'coach', 'guru', 'mentor', 'instrutor', 'consultor', 'especialista', 'conselheiro', 'orientador', 'facilitador', 'moderador', 'animador', 'comentarista', 'narrador de documentário', 'narrador de podcast', 'narrador de audiobook'));
